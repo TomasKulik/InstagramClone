26a68a2778c4dbce66bd87cbadb8cf4540a785ce
Karel <Karel4ever@gmail.com>
2020-03-16 23:48:56 +0100

    Added Firebase authentication

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/feed_screen.dart';
+
+class AuthService {
+  static final _auth = FirebaseAuth.instance;
+  static final _firestore = Firestore.instance;
+
+  static void signUpUser(
+      BuildContext context, String name, String email, String password) async {
+    try {
+      AuthResult authResult = await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      FirebaseUser signedInUser = authResult.user;
+      if (signedInUser != null) {
+        _firestore.collection('/users').document(signedInUser.uid).setData(
+          {
+            'name': name,
+            'email': email,
+            'profileImageUrl': '',
+          },
+        );
+        Navigator.pushReplacementNamed(context, FeedScreen.id);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static void logout() {
+    _auth.signOut();
+  }
+
+  static void login(String email, String password) async {
+    _auth.signInWithEmailAndPassword(
+      email: email.trim(),
+      password: password,
+    );
+  }
+}
