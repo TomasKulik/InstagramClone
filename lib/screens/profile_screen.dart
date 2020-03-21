ca8e5b19eef15238a8aabe157cff5558728bd441
Karel <Karel4ever@gmail.com>
2020-03-21 18:59:23 +0100

    Added profile and editProfile screens, Created model for user and ability to update name and bio

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user_model.dart';
+import 'package:instagram_clone/screens/edit_profile_screen.dart';
+import 'package:instagram_clone/utilities/constants.dart';
 
 class ProfileScreen extends StatefulWidget {
+  final String userId;
+
+  ProfileScreen({this.userId});
+
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
@@ -9,10 +16,135 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'ProfileScreen',
-        ),
+      backgroundColor: Colors.white,
+      body: FutureBuilder(
+        future: userRef.document(widget.userId).get(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          User user = User.fromDoc(snapshot.data);
+          return ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 30.0),
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: NetworkImage(
+                          'https://image.news.livedoor.com/newsimage/stf/2/e/2ec2e_50_0624eb7a_eb70f6ec-cm.jpg?v=20180831225629'),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    '69',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    'posts',
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    '420',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    'followers',
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  Text(
+                                    '88',
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    'following',
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Container(
+                            width: 200.0,
+                            child: FlatButton(
+                              onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => EditProfileScreen(user: user),
+                                ),
+                              ),
+                              color: Colors.blue,
+                              textColor: Colors.white,
+                              child: Text(
+                                'Edit Profile',
+                                style: TextStyle(fontSize: 18.0),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30.0,
+                  vertical: 10.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      user.name,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Container(
+                      height: 80.0,
+                      child: Text(
+                        user.bio,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/screens/feed_screen.dart';
 
 class AuthService {
   static final _auth = FirebaseAuth.instance;
@@ -23,7 +22,7 @@ class AuthService {
             'profileImageUrl': '',
           },
         );
-        Navigator.pushReplacementNamed(context, FeedScreen.id);
+        Navigator.pop(context);
       }
     } catch (e) {
       print(e);
